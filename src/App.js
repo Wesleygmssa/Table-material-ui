8adf18b57b1bb8949176b4024b245a8e467678e6
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-13 23:31:50 -0300

    update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Paper from "@material-ui/core/Paper";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { Container } from "./styles";
+import TablePagination from "@material-ui/core/TablePagination";
 
 const useRowStyles = makeStyles({
   root: {
@@ -92,6 +93,15 @@ export default function App() {
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={100}
+        rowsPerPage={5}
+        page={3}
+        onChangePage={() => {}}
+        onChangeRowsPerPage={() => {}}
+      />
     </Container>
   );
 }
