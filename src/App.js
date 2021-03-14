ffbb4f566a3b4b691ef153c285b126eef20c1748
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-13 23:19:07 -0300

    cretae table

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import { Container } from "./styles";
 
 const useRowStyles = makeStyles({
   root: {
@@ -22,21 +23,6 @@ const useRowStyles = makeStyles({
     },
   },
 });
-
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      { date: "2020-01-05", customerId: "11091700", amount: 3 },
-      { date: "2020-01-02", customerId: "Anonymous", amount: 1 },
-    ],
-  };
-}
 
 function Row(props) {
   const { row } = props;
@@ -55,50 +41,56 @@ function Row(props) {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell className="ContentRegister">2020.001.000155/RG</TableCell>
+        <TableCell>Salvador</TableCell>
+        <TableCell>COADE</TableCell>
+        <TableCell>Fauna</TableCell>
+        <TableCell>19/01/2021</TableCell>
+        <TableCell>Vinculado</TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
+              <Typography
+                className="boxContent"
+                variant="h6"
+                gutterBottom
+                component="div"
+              >
+                Derramamento de óleo na pista
               </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
             </Box>
           </Collapse>
         </TableCell>
       </TableRow>
     </React.Fragment>
+  );
+}
+
+export default function App() {
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell className="title">Nº do Registro</TableCell>
+              <TableCell className="title">Município</TableCell>
+              <TableCell className="title">Área</TableCell>
+              <TableCell className="title">Eixo Temático</TableCell>
+              <TableCell className="title">Data do Registro</TableCell>
+              <TableCell className="title">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <Row />
+            <Row />
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
 
@@ -119,35 +111,3 @@ Row.propTypes = {
     protein: PropTypes.number.isRequired,
   }).isRequired,
 };
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-  createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
-];
-
-export default function CollapsibleTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,24 @@
-import styled from "react";
+import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .boxContent {
+    display: inline-block;
+    margin-left: 70px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 160%;
+    letter-spacing: 0.25px;
+    color: #757575;
+  }
+  .ContentRegister {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 160%;
+    letter-spacing: 0.25px;
+    color: #006fda;
+  }
+`;
